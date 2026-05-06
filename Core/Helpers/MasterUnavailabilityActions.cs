ba10cdebb5cd94d30089740b217476164b47c0f3
Stanley Ivan Sandjo <heboivan@gmail.com>
2026-05-06 13:51:37 +0200

    Created Root.Core.Helpers

---
+++ Core/Helpers/MasterUnavailabilityActions.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace Root.Core.Helpers;
+
+public class MasterUnavailabilityActions {
+
+}
--- Utils/MasterUnavailabilityActions.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Root.Utils;
-
-public class MasterUnavailabilityActions
-{
-
-}
